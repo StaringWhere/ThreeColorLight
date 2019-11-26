3e8818aee29d5bc98df2dc528bab298db873ba6a
Staring Where <949047497@163.com>
2019-11-26 23:22:45 +0800

    Recording fixed

--- Recording.v
+++ Recording.v
@@ -26,7 +26,7 @@ input Div_CLK;
 reg Tenmili_CLK; //T = 0.01s
 reg [7:0]Tenmili_Cnt;
 reg [19:0]record_switch;
-reg [19:0]record_duration[7:0];
+reg [7:0]record_duration[19:0]; //20个8位向量
 reg [4:0]index;
 
 initial
@@ -35,14 +35,8 @@ begin
 	Tenmili_Cnt = 8'd0;
 	record_switch[19:1] = 0;
 	record_switch[0] = 0;
-	record_duration[7] = 20'd0; //数组不能多个一起赋值，向量可以
-	record_duration[6] = 20'd0;
-	record_duration[5] = 20'd0;
-	record_duration[4] = 20'd0;
-	record_duration[3] = 20'd0;
-	record_duration[2] = 20'd0;
-	record_duration[1] = 20'd0;
-	record_duration[0] = 20'd0;
+	for(index = 0; index < 20; index = index + 1) //数组不能多个一起赋值，向量可以
+		record_duration[index] = 8'd0;
 	index = 5'd0;
 end
 
@@ -62,7 +56,10 @@ always@(posedge Div_CLK) //录制按键以及改变index 每0.1ms一次
 begin
 	if(fake_switch != record_switch[index])
 	begin
-		index = index + 1;
+		if(index == 5'd19)
+			index = 5'd0;
+		else 
+			index = index + 1;
 		record_switch[index] = fake_switch;
 	end
 end
